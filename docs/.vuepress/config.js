d09f672c5e8f9ba37ecb2192674ddb4ac77eb6e3
海立婷 <hailiting@yeah.net>
2019-06-27 17:14:18 +0800

    暂存

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,28 +10,106 @@ module.exports = {
         sidebar: [
             '/',
             {
-                title: 'JavaScript',
+                title: '计算机服务基础',
+                collapsable: true,
+                children: [
+                    '/计算机服务基础/express'
+                ]
+            },
+            {
+                title: 'HTML',
+                collapsable: true,
+                children: [
+                    '/HTML/express'
+                ]
+            },
+            {
+                title: 'CSS',
+                collapsable: true,
+                children: [
+                    '/CSS/express'
+                ]
+            },
+            {
+                title: 'ECMAScript',
                 collapsable: true,
                 children:[
-                    '/JavaScript/one',
-                    '/JavaScript/two'
+                    '/ECMAScript/one',
+                    '/ECMAScript/two'
                 ]
             },
             {
-                title: 'notes',
+                title: 'TypeScript',
                 collapsable: true,
-                children: [
-                    '/notes/nodejs',
-                    '/notes/express',
+                children:[
+                    '/TypeScript/express',
                 ]
             },
             {
-                title: 'dataStructure',
+                title: 'NodeJS',
                 collapsable: true,
                 children: [
-                    '/dataStructure/one'
+                    '/NodeJS/nodejs',
+                    '/NodeJS/express',
                 ]
-            }
+            },
+            {
+                title: '算法与数据结构',
+                collapsable: true,
+                children: [
+                    '/算法与数据结构/one'
+                ]
+            },
+            {
+                title: '浏览器',
+                collapsable: true,
+                children: [
+                    '/浏览器/express'
+                ]
+            },
+            {
+                title: '前端框架',
+                collapsable: true,
+                children: [
+                    '/前端框架/express',
+                ]
+            },
+            {
+                title: '小程序',
+                collapsable: true,
+                children: [
+                    '/Wechat/express'
+                ]
+            },
+            {
+                title: '微信公众号',
+                collapsable: true,
+                children: [
+                    '/微信公众号/express'
+                ]
+            },
+            {
+                title: '工程化',
+                collapsable: true,
+                children: [
+                    '/工程化/express'
+                ]
+            },
+            {
+                title: 'Flutter',
+                collapsable: true,
+                children: [
+                    '/Flutter/express'
+                ]
+            },
+            {
+                title: 'ReactNative',
+                collapsable: true,
+                children: [
+                    '/ReactNative/express'
+                ]
+            },
+            
         ]
 
     }
