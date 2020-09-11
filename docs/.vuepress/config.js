ceb367404c306e5d54f38349cf17c00c286e3ad5
hai.liting <hailiting@example.com>
2020-09-11 11:12:11 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,18 @@
-var MarkdownIt = require('markdown-it');
+var MarkdownIt = require("markdown-it");
 var md = new MarkdownIt();
-md.use(require('markdown-it-anchor'));
-md.use(require('markdown-it-table-of-contents'));
+md.use(require("markdown-it-anchor"));
+md.use(require("markdown-it-table-of-contents"));
 
-
-const path = require("path")
-const rootpath = path.dirname(__dirname) //执行一次dirname将目录定位到docs目录
-const filenames = require('./filenames.json');
-
+const path = require("path");
+const rootpath = path.dirname(__dirname); //执行一次dirname将目录定位到docs目录
+const filenames = require("./filenames.json");
 
 module.exports = {
-    title: 'hailiting Blog',
-    description: 'hailiting Blog',
-    base: '/hai_blog/',
-    themeConfig: {
-        sidebar: filenames
-    }
-}
+  title: "hailiting Blog",
+  description: "hailiting Blog",
+  base: "/hai_blog/",
+  dest: "public",
+  themeConfig: {
+    sidebar: filenames,
+  },
+};
