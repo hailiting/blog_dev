ad5a6b9370e4a917974da7238e10c05ff34134d5
hai.liting <hailiting@example.com>
2021-08-17 09:22:27 +0800

    houdini

---
+++ demo/Houdini/arc.js
@@ -0,0 +1,17 @@
+if (typeof registerPaint !== "undefined") {
+  registerPaint(
+    "background-canvas",
+    class {
+      static get inputProperties() {
+        return ["--background-canvas"];
+      }
+      paint(ctx, geom, properties) {
+        eval(properties.get("--background-canvas").toString())(
+          ctx,
+          geom,
+          properties
+        );
+      }
+    }
+  );
+}
