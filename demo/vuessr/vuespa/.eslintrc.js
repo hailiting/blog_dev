3ea47c806c1363232aa8cede89044bb2b94c2cc0
hai.liting <hailiting@example.com>
2021-11-02 15:40:42 +0800

    暂存

--- demo/vuessr/vuespa/.eslintrc.js
+++ demo/vuessr/vuespa/.eslintrc.js
@@ -3,27 +3,28 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    "plugin:vue/essential",
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    "standard"
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ["vue"],
   // add your custom rules here
   rules: {
+    eqeqeq: 0, // 必须使用全等
+    quotes: [1, "double"], // 引号类型 `` "" ''
+    semi: 0, // 语句可以需要分号结尾
     // allow async-await
-    'generator-star-spacing': 'off',
+    "generator-star-spacing": "off",
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
-}
+};
--- demo/vuessr/vuespa/src/components/HelloWorld.vue
+++ demo/vuessr/vuespa/src/components/HelloWorld.vue
@@ -1,94 +1,16 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="hello-bem hello-bem--init">
+    <button class="hello-bem__btn">加1 +1</button>
+    <button class="hello-bem__btn">减1 -1</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App"
     }
   }
 }
@@ -96,18 +18,22 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.hello-bem{
+  width: 300px;
+  margin: 0 auto;
+  border: 1px solid rgb(99, 61, 61);
+  border-radius: 20px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.hello-bem__btn{
+  background-color: #e89c0c;
+  color: #fff;
+  border: none;
+  padding: 6px 20px;
+  font-weight: bold;
+  font-size: 14px;
+  border-radius: 4px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hello-bem--init{
+  padding: 1em;
 }
 </style>
